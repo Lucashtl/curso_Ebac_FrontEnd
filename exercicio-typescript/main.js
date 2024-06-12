130e8347475c7cd3ed512de675d66285675f8ded
Lucas Fernandes <lucasuzumaki69@hotmail.com>
2024-06-12 17:40:41 -0300

    entrega do exercicio typescript

---
+++ exercicio-typescript/main.js
@@ -0,0 +1,10 @@
+"use strict";
+function multiplicar(numero1, numero2) {
+    return numero1 * numero2;
+}
+function dizOi(nome) {
+    nome = nome;
+    return `${nome} diz olá`;
+}
+console.log(dizOi('Lucas'));
+console.log(multiplicar(5, 8));
---
+++ exercicio-typescript/main.ts
@@ -0,0 +1,11 @@
+function multiplicar (numero1:number, numero2: number) {
+    return numero1 * numero2;
+}
+
+function dizOi (nome:string){
+    nome = nome
+    return `${nome} diz olá`
+}
+
+console.log(dizOi('Lucas'))
+console.log(multiplicar(5, 8))
